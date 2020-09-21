cb03d8e37c03e5cb525409fd9c208cfeda0581d3
ayushi <ayushishri1994@gmail.com>
2020-09-21 16:01:45 +0530

    first commit

---
+++ src/test/java/qaclickacademy/Mavenjava/AppiumTest.java
@@ -0,0 +1,19 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void nativeappandroid()
+	{
+System.out.println("nativeappandroid");
+	}
+
+	@Test
+	public void IOSapps()
+	{
+		System.out.println("IOSapps");
+	}
+
+	}
+
---
+++ src/test/java/qaclickacademy/Mavenjava/RestAPITest.java
@@ -0,0 +1,17 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+	@Test
+	public void Postjira()
+	{
+System.out.println("Postjira");
+	}
+
+	@Test
+	public void Deletetwitter()
+	{
+		System.out.println("Deletetwitter");
+	}
+}
